1196a85569e6bfac64dc39436b5f918a91f60385
WanderleiSI <wanderleirochavasco@hotmail.com>
2022-10-03 11:31:37 -0300

    1 pull dia 03

--- pyssword.py
+++ pyssword.py
@@ -8,20 +8,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
 
 class MeuLayout(FloatLayout):
-    usuario = ObjectProperty(None)
-    senha = ObjectProperty(None)
-
-    def fazerLogin(self):
-        usuario = self.usuario.text
-        senha = self.senha.text
-        if usuario == "mauricio" and senha == "senha123":
-            print("Entrou!!!")
-        else:
-            print("Sai fora!!!")
-
-        self.usuario.text = ""
-        self.senha.text = ""
-
+    pass
 class PysswordApp(App):
     def build(self):
         return MeuLayout()
